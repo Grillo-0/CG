223426e8ac59682f9a7500a3ef2ef919c0ab690d
Arthur Grillo <arthurgrillo@riseup.net>
2024-04-13 20:44:03 -0300

    Remove the light model (for now)
    Its better to not have this weird lighting

--- resources/shaders/frag.glsl
+++ resources/shaders/frag.glsl
@@ -8,6 +8,5 @@ out vec4 FragColor;
 uniform sampler2D tex;
 
 void main() {
-	float mag = dot(normalize(norm_frac), vec3(1.0,1,-1.0));
-	FragColor = mix(texture(tex, uv_frac), vec4(0.1, 0.1,0.1,1), mag);
+	FragColor = texture(tex, uv_frac);
 }
